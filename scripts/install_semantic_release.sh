95b06bb39870f771a625d4935ee908e67ab8780f
Danny Julian Murcia <dannymurcia1@gmail.com>
2025-02-25 16:11:38 -0500

    feat: Update install_semantic_release.sh

--- scripts/install_semantic_release.sh
+++ scripts/install_semantic_release.sh
@@ -3,8 +3,8 @@
 VAR_NAME_ROOT_REPOSITORY=$(echo $VAR_NAME_REPOSITORY | cut -d'/' -f2)
 cd ..
 mkdir -p $VAR_NAME_ROOT_REPOSITORY/semantic-release/
-cp $OFL "toolkit-demo/files/semantic-release/create-release-branch.js" "$VAR_NAME_ROOT_REPOSITORY/semantic-release/create-release-branch.js"
-cp $OFL "toolkit-demo/files/semantic-release/release.config.js" "$VAR_NAME_ROOT_REPOSITORY/release.config.js"
-cp $OFL "toolkit-demo/files/semantic-release/release-rules.js" "$VAR_NAME_ROOT_REPOSITORY/semantic-release/release-rules.js"
-cp $OFL "toolkit-demo/files/semantic-release/writerChangelog.js" "$VAR_NAME_ROOT_REPOSITORY/semantic-release/writerChangelog.js"
-cp $OFL "toolkit-demo/files/semantic-release/semantic-release.yml" "$VAR_NAME_ROOT_REPOSITORY/.github/workflows/semantic-release.yml"
+cp $OFL "$VAR_LOCAL_PATH/files/semantic-release/create-release-branch.js" "$VAR_NAME_ROOT_REPOSITORY/semantic-release/create-release-branch.js"
+cp $OFL "$VAR_LOCAL_PATH/files/semantic-release/release.config.js" "$VAR_NAME_ROOT_REPOSITORY/release.config.js"
+cp $OFL "$VAR_LOCAL_PATH/files/semantic-release/release-rules.js" "$VAR_NAME_ROOT_REPOSITORY/semantic-release/release-rules.js"
+cp $OFL "$VAR_LOCAL_PATH/files/semantic-release/writerChangelog.js" "$VAR_NAME_ROOT_REPOSITORY/semantic-release/writerChangelog.js"
+cp $OFL "$VAR_LOCAL_PATH/files/semantic-release/semantic-release.yml" "$VAR_NAME_ROOT_REPOSITORY/.github/workflows/semantic-release.yml"
